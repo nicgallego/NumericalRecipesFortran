3f6bb82fef8a5ec7c1053adb77e836a6ca5ba610
Nicolas Gallego <nicgallego@gmail.com>
2025-05-12 23:01:52 +0200

    test harness collapse
    - after running badluck, expected to fail, the test harness does not
      detect any failing testes anymore
    - at least caldat with wrong data is passing
    - this adds to issue #8

--- tests/tester.f90
+++ tests/tester.f90
@@ -8,6 +8,7 @@ program tester
    use test_julday, only : collect_julday
    use test_date_utils, only: collect_date_utils
    use test_badluck, only: collect_badluck
+   use test_caldat, only: collect_caldat
    !> add here tests as we go and append to the end of testsuites below
    implicit none
 
