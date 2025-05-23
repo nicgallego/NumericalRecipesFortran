563f7fe413c744e8db127befd3a7b6f6de5aa69d
Nicolas Gallego <nicgallego@gmail.com>
2025-05-23 14:07:39 +0200

    source also classified by chapter subfolders
    - the idea is to have a module per folder, and keep them independent
      among each other
    - adds to sorting code around to proceed testing ch2 content. Issue #23

---
+++ tests/ch2/ch2_tester.f90
@@ -0,0 +1,11 @@
+!> @brief entrypoint for tests in chapter 2
+
+program ch2_tester
+   use, intrinsic :: iso_fortran_env, only: error_unit
+   use testdrive, only : run_testsuite, new_testsuite, testsuite_type
+   use test_gaussj, only: collect_gaussj
+   !> append here as we go
+   implicit none
+
+end program ch2_tester
+
---
+++ tests/ch2/test_gaussj.f90
@@ -0,0 +1,4 @@
+!> @brief Gauss Jordan method test suite
+module test_gaussj
+
+end module test_gaussj
