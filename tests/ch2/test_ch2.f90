89b6ac4d52c029bd392cd9fb743dc0a26fa320bf
Nicolas Gallego <nicgallego@gmail.com>
2025-05-23 13:26:59 +0200

    Tests organized by chapter
    - every subfolder of tests contains the test suite(s) and benchmark(s)
      related to its content.
    - serves as preparation to create the first test of ch2. issue #23

---
+++ tests/ch2/test_ch2.f90
@@ -0,0 +1,16 @@
+!> @brief ch2 Linear equations solvers tests
+!>
+module test_ch2
+
+   use testdrive, only: new_unittest, unittest_type, error_type, check, test_failed
+   use ch2, only: gaussj
+   implicit none
+   private
+
+   public :: collect_ch2
+
+   contains
+
+
+end module test_ch2
+
